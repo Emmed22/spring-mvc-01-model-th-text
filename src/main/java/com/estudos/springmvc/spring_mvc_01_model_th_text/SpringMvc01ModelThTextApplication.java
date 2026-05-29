c4b732a6552f72e3af59da4e87a3b1336a12605c
Emmed22 <duartmary22006@gmail.com>
2026-05-29 09:44:18 -0300

    initial commit: spring boot project setup

---
+++ src/main/java/com/estudos/springmvc/spring_mvc_01_model_th_text/SpringMvc01ModelThTextApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.springmvc.spring_mvc_01_model_th_text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc01ModelThTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc01ModelThTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/springmvc/spring_mvc_01_model_th_text/SpringMvc01ModelThTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.springmvc.spring_mvc_01_model_th_text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc01ModelThTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

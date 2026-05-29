016e96b33e0811468b81a29db5a01d73af39c6fc
Emmed22 <duartmary22006@gmail.com>
2026-05-29 10:23:19 -0300

    feat: add Model and the th:text example in MensagemController

---
+++ src/main/java/com/estudos/springmvc/spring_mvc_01_model_th_text/controllers/MensagemController.java
@@ -0,0 +1,26 @@
+package com.estudos.springmvc.spring_mvc_01_model_th_text.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// 1. Você acessa /mensagem
+@Controller
+public class MensagemController {
+    // 2. Spring cria uma Request
+    @GetMapping("/mensagem")
+    // 3. Spring cria um Model vazio
+    public String mostrarMensagem(Model model) {
+        // 4. Controller preenche o Model:
+        // - nome = Mari
+        model.addAttribute("nome", "Mari");
+        // - curso = DSM
+        model.addAttribute("curso", "DSM");
+        // 5. Spring envia isso para o HTML
+        // 6. Thymeleaf lê ${nome} e ${curso}
+        // 7. Página aparece pronta
+        return ("mensagem");
+
+    }
+
+}
